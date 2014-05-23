5dfe1715e1a5063c93d8e3229fa5865fbe3f4305
Demis Bellot <demis.bellot@gmail.com>
2014-05-23 00:15:57 -0400

    Add Xamarin.sln and BenchmarksAnalyzer.Mac OSX UI

---
+++ src/BenchmarksAnalyzer/BenchmarksAnalyzer.Mac/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BenchmarksAnalyzer.Mac
+{
+    // Should subclass MonoMac.AppKit.NSResponder
+    [MonoMac.Foundation.Register ("AppDelegate")]
+    public partial class AppDelegate
+    {
+    }
+}
+
---
+++ src/BenchmarksAnalyzer/BenchmarksAnalyzer.Mac/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace BenchmarksAnalyzer.Mac
+{
+    public static class MainClass
+    {
+        public static AppHost App;
+
+        static void Main (string[] args)
+        {
+            App = new AppHost("http://localhost:1337/")
+                .Start();
+
+            NSApplication.Init ();
+            NSApplication.Main (args);
+        }
+    }
+}
+
